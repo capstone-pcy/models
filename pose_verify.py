11ea0ab622f01e07a04c087596d145575f679ea7
youngminChoi <martinus.choi@gmail.com>
2021-05-26 02:08:11 +0900

    pose verify log version 1 -> error

--- pose_verify.py
+++ pose_verify.py
@@ -1,10 +1,12 @@
 import cv2
 import os
+import math
+import json
 import mediapipe as mp
 
 input_dir = "data/video"
 output_dir = "data/output_video/pose_estimate"
-log_file_dir = "data/log/pose_log"
+log_file_dir = "data/log/pose_log/"
 
 def main():
     mp_drawing = mp.solutions.drawing_utils
@@ -20,6 +22,17 @@ def main():
 
         FPS = cap.get(cv2.CAP_PROP_FPS)
         delay = round(1000/FPS)
+        frame_second = 0
+
+        os.makedirs(log_file_dir + file_name, exist_ok=True)
+        log_file_path = os.path.join(log_file_dir, file_name + '/')
+        face_log_name = file_name + '-face_verify_log.json'
+        rhand_log_name = file_name + '-rhand_verify_log.json'
+        lhand_log_name = file_name + '-lhand_verify_log.json'
+        face_log_path = os.path.join(log_file_path, face_log_name)
+        rhand_log_path = os.path.join(log_file_path, rhand_log_name)
+        lhand_log_path = os.path.join(log_file_path, lhand_log_name)
+
 
         make_dir = os.path.join(output_dir, file_name)
         os.makedirs(make_dir, exist_ok=True)
@@ -39,6 +52,12 @@ def main():
 
                 if not ret : break
 
+                frame_second += (1 / FPS)
+
+                right_hand_logs = dict()
+                left_hand_logs = dict()
+                face_logs = dict()
+
                 # Recolor feed
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         
@@ -53,31 +72,81 @@ def main():
                 # Draw face landmarks
                 if results.face_landmarks:
                     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+                    face_logs[frame_second] = 0
                 else:
-                    print("No face!")
+                    face_logs[frame_second] = 1
 
         
                 # Draw right hand landmarks
                 if results.right_hand_landmarks:
                     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                    right_hand_logs[frame_second] = 0
                 else:
-                    print("NO right hand!")
+                    right_hand_logs[frame_second] = 1
         
 
                 # Draw left hand landmarks
                 if results.left_hand_landmarks:
                     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                    left_hand_logs[frame_second] = 0
                 else:
-                    print("NO left hand!")
+                    left_hand_logs[frame_second] = 1
         
         
                 cv2.imshow("image", image)
                 writer.write(image)
 
-                if cv2.waitKey(10) & 0xFF == ord('q') : break
+                if cv2.waitKey(delay) & 0xFF == ord('q') : break
 
         cap.release()
         cv2.destroyAllWindows()
+
+        cur_sec = 0
+        avg = 0
+
+        sec_wise_face = dict()
+        sec_wise_rhand = dict()
+        sec_wise_lhand = dict()
+
+        for sec, value in face_logs.items():
+            if cur_sec < math.floor(sec):
+                sec_wise_face[cur_sec] = round(avg/FPS)
+                cur_sec = math.floor(sec)
+                avg = 0
+            else:
+                avg += value
+        
+        cur_sec = 0
+
+        for sec, value in right_hand_logs.items():
+            if cur_sec < math.floor(sec):
+                sec_wise_rhand[cur_sec] = round(avg/FPS)
+                cur_sec = math.floor(sec)
+                avg = 0
+            else:
+                avg += value
+         
+        cur_sec = 0
+
+        for sec, value in left_hand_logs.items():
+            if cur_sec < math.floor(sec):
+                sec_wise_lhand[cur_sec] = round(avg/FPS)
+                cur_sec = math.floor(sec)
+                avg = 0
+            else:
+                avg += value
+        
+
+        with open(face_log_path, 'w') as outfile:
+            json.dump(sec_wise_face, outfile)
+        
+        with open(rhand_log_path, 'w') as outfile:
+            json.dump(sec_wise_rhand, outfile)
+        
+        with open(lhand_log_path, 'w') as outfile:
+            json.dump(sec_wise_lhand, outfile)
+        
+
 
 if __name__ == '__main__':
     main()
